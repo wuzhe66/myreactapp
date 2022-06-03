b7ddebae70021d00d881b7dd9994e7cc34ba3179
wuzhe66 <91699733+wuzhe66@users.noreply.github.com>
2022-06-03 16:59:43 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [color, setColor] = React.useState(
+    JSON.parse(sessionStorage.getItem("color")) || "blue"
+);
+const [count, setCount] = React.useState(
+    JSON.parse(localStorage.getItem("count")) || 0
+  );
+
+  React.useEffect(() => {
+    sessionStorage.setItem("color", JSON.stringify(color));
+localStorage.setItem("count", JSON.stringify(count));
+
+  }, [color,count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DisplayMessage color={color} />
+      <Clock time={new Date().toLocaleTimeString()} />
+      <button
+        onClick={() => {
+          setColor(toggle(color));
+          setCount(count + 1);
+        }}
+      >
+        Click me React
+      </button>
+      <p>count:{count}</p>
     </div>
   );
 }
+
+//写法二
+class Main2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "blue",
+      count: 0,
+    };
+  }
+
+  updateColorAndCount = () => {
+    const newColor = this.state.color === "blue" ? "red" : "blue";
+    const newCount = this.state.count + 1;
+
+    this.setState({
+      color: newColor,
+      count: newCount,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <DisplayMessage color={this.state.color} />
+        <Clock time={new Date().toLocaleTimeString()} />
+        <button onClick={this.updateColorAndCount}>Click me React</button>
+        <p>{this.state.count}</p>
+      </div>
+    );
+  }
+}
+
+function toggle(color) {
+  if (color === "blue") {
+    return "red";
+  } else {
+    return "blue";
+  }
+}
+
+//component
+function DisplayMessage(props) {
+  return <h1 style={{ color: props.color }}>Hello React!</h1>;
+}
+
+//props
+function Clock(props) {
+  return <p>React Clock:{props.time}</p>;
+}
+
+root.render(<Main />);
 
 export default App;
